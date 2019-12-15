0d1b97b4ab0dc02a30414045fc707790d73f5bcb
Mo Saleh <mohamed.is8890@gmail.com>
2019-12-15 23:01:33 +0200

    Implemented useMemo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import ReactDOM from "react-dom";
 import Button from "./Button";
 
@@ -10,6 +10,7 @@ const functions = new Set();
 const App = () => {
   const [delta, setDelta] = useState(1);
   const [c, setC] = useState(0);
+  const [sinC, setSinC] = useState(0);
 
   const incrementDelta = useCallback(() => setDelta(delta => delta + 1), []);
   const increment = useCallback(() => setC(c => c + delta), [delta]);
@@ -20,6 +21,11 @@ const App = () => {
     increment();
   }, [increment, incrementDelta]);
 
+  // This value will not be recomputed between re-renders
+  // unless the value of c changes
+  // Notice that it operates though it is never called
+  const sinOfC = useMemo(() => setSinC(Math.sin(c)), [c]);
+
   // Register the functions so we can count them
   functions.add(incrementDelta);
   functions.add(increment);
@@ -28,6 +34,7 @@ const App = () => {
     <div>
       <div> Delta is {delta} </div>
       <div> Counter is {c} </div>
+      <div> Sin of Counter is {sinC} </div>
       <br />
       <div>
         <Button onClick={incrementDelta}>Increment Delta</Button>
